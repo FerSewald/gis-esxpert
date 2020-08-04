3c61d61ec94946a76fd1c568477ddd49a2d21ca7
Fernando Sewald <fersewal@gmail.com>
2020-08-04 10:00:01 -0300

    docs agregado

---
+++ docs/precache-manifest.6b356b68af37902393a0fafd44b70d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530b850811c04c5284d9817f5d7686dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28913c57757fc8683df1",
+    "url": "/static/css/main.6fd9cfff.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28913c57757fc8683df1",
+    "url": "/static/js/main.746e9bad.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
